b0cec04d0ca15578999fe5be93cde805f02d75f6
hobglobin <luizfil98@gmail.com>
2017-01-23 14:01:54 -0200

    Part2
    Desenvolvimento

--- app/src/main/java/teste/agendacontato/ActCadContatos.java
+++ app/src/main/java/teste/agendacontato/ActCadContatos.java
@@ -18,27 +18,107 @@ public class ActCadContatos extends AppCompatActivity
     private EditText edtDatasEspeciais;
     private EditText edtGrupos;
 
+
     private Spinner spnTipoEmail;
     private Spinner spnTipoTelefone;
     private Spinner spnTipoEndereco;
     private Spinner spnTipoDatasEspeciais;
 
+    private ArrayAdapter<String> adtTipoEmail;
+    private ArrayAdapter<String> adtTipoEndereco;
+    private ArrayAdapter<String> adtTipoTelefone;
+    private ArrayAdapter<String> adtTipoDatasEspeciais;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_cad_contatos);{
+        setContentView(R.layout.act_cad_contatos);
 
-        }
+
+       /* edtNome=(EditText)findViewById(R.id.edtNome);
+        edtEndereco=(EditText)findViewById(R.id.edtEndereco);
+        edtEmail=(EditText)findViewById(R.id.edtEmail);
+        edtDatasEspeciais=(EditText)findViewById(R.id.edtDatasEspeciais);
+        edtGrupos=(EditText)findViewById(R.id.edtGrupos);  esse pedaco ta fazendo crashar  */
+
+        spnTipoEmail=(Spinner)findViewById(R.id.spnTipoEmail);
+        spnTipoTelefone=(Spinner)findViewById(R.id.spnTipotelefone);
+        spnTipoEndereco=(Spinner)findViewById(R.id.spnTipoEndereco);
+        spnTipoDatasEspeciais=(Spinner)findViewById(R.id.spnDatasEspeciais);
+
+
+        adtTipoEmail= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        adtTipoTelefone= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        adtTipoEndereco= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+        adtTipoDatasEspeciais= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item);
+
+        adtTipoEmail.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adtTipoEndereco.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adtTipoTelefone.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adtTipoDatasEspeciais.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+
+        spnTipoEmail.setAdapter(adtTipoEmail);
+        spnTipoEndereco.setAdapter(adtTipoEndereco);
+        spnTipoTelefone.setAdapter(adtTipoTelefone);
+        spnTipoDatasEspeciais.setAdapter(adtTipoDatasEspeciais);
+
+
+        adtTipoEmail.add("Casa");
+        adtTipoEmail.add("Trabalho");
+        adtTipoEmail.add("Outros");
+
+
+        adtTipoTelefone.add("Celular");
+        adtTipoTelefone.add("Casa");
+        adtTipoTelefone.add("Trabalho");
+        adtTipoTelefone.add("Outros...?");
+
+        adtTipoEndereco.add("Casa");
+        adtTipoEndereco.add("Trabalho");
+        adtTipoEndereco.add("Outros");
+
+
+        adtTipoDatasEspeciais.add("Birthday");
+        adtTipoDatasEspeciais.add("Data comemorativa");
+        adtTipoDatasEspeciais.add("outros");
+
+
 
 
 
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu_act_cad_contatos,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId())
+
+        {
+                case R.id.mni_acao1:
+
+                break;
+
+                case R.id.mni_acao2:
+
+                break;
+
+
+        }
 
 
 
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/teste/agendacontato/ScriptSQL.java
@@ -0,0 +1,34 @@
+package teste.agendacontato;
+
+/**
+ * Created by LuizFilipeFerreira on 1/23/2017.
+ */
+
+public class ScriptSQL {
+public static String getCreateconato()
+{
+
+
+   StringBuilder sqlbuilder= new StringBuilder();
+    sqlbuilder.append("CREATE TABLE IF NOT EXISTS CONTATO ( ");
+    sqlbuilder.append(" _id                INTEGER       NOT NULL ");
+    sqlbuilder.append("    PRIMARY KEY AUTOINCREMENT, ");
+    sqlbuilder.append("        TELEFONE           VARCHAR (14), ");
+    sqlbuilder.append("TIPOTELEFONE       VARCHAR (1), ");
+    sqlbuilder.append("EMAIL              VARCHAR (255), ");
+    sqlbuilder.append("TIPOEMAIL          VARCHAR (1), ");
+    sqlbuilder.append("ENDERECO           VARCHAR (255), ");
+    sqlbuilder.append("TIPOENDERECO       VARCHAR (1), ");
+    sqlbuilder.append("DATASESPECIAIS     DATE,");
+    sqlbuilder.append("TIPODATASESPECIAIS VARCHAR (1), ");
+    sqlbuilder.append("GRUPOS             VARCHAR (255) ");
+    sqlbuilder.append("); ");
+
+    return sqlbuilder.toString();
+
+
+
+
+
+}
+}
